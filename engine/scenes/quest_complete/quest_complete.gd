7d65462ff9a404ced3f4888601be555229530c1d
Plet53 <Plet53@thegame.com>
2026-01-13 21:43:25 -0700

    Quest Complete Screen Layout, Text

---
+++ engine/scenes/quest_complete/quest_complete.gd
@@ -0,0 +1,45 @@
+@tool
+extends Control
+class_name QuestComplete
+
+# ==============================================================================
+
+# ==============================================================================
+@onready var title = $"QuestComplete"
+@onready var summary_label = $"SummaryLabel"
+@onready var score_label = $"ScoreLabel"
+@onready var monster_rect = $"MonsterTaunt/MonsterSprite"
+@onready var monster_text = $"MonsterTaunt/MonsterText"
+# ==============================================================================
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	title.text = tr("quest-finished.title")
+	var quest = Quest.get_current()
+	var summary_values := {
+		"quest_name": quest.name,
+		"difficulty_name": quest.source_difficulty.name
+	}
+	summary_label.text = tr("quest-finished.summary").format(summary_values)
+	score_label.text = tr("quest-finished.score").format({"score": quest.get_attributes().score})
+	
+	var stages = quest.get_stages().filter(func (stage):
+		return not stage is SpecialStage
+	)
+	var random_stage = stages[randi() % stages.len()]
+	var random_theme = random_stage.get_theme()
+	
+	monster_rect.texture = random_theme.get_icon("monster", "Cell")
+	monster_text.text = load("res://assets/string_tables/monster_taunts.tres").pick_random().format({"monster": random_stage._generate_monster_name().to_upper()})
+
+# Anim Sequence:
+# Quest Complete scrolls in from the side
+# Each Line of Summary Fades in and scrolls from just below
+# Monster Pops In, expanding and then bouncing back to normal size a few times
+# Monster's Taunt scrolls in 1 character per frame
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+# High Score Popup
+# New Quest Popup
